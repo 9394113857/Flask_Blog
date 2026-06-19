98be07bc90f9263638dd9718e86fc7ffff1e900a
Raghunadh_9394113857 <barlapudiraghunadh9@gmail.com>
2026-06-19 16:50:50 +0530

    Remove merge conflict markers from run.py

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     
     
 # venv\Scripts\activate
-<<<<<<< HEAD
 # set FLASK_APP=run.py
 =======
 # $env:FLASK_APP="run.py"
@@ -27,11 +26,11 @@ if __name__ == "__main__":
 
 # # Start Flask Application ✅✅  
 # $env:FLASK_APP="run.py" # Set the FLASK_APP environment variable to run.py
-<<<<<<< HEAD
+
 # flask run --host=0.0.0.0 --port=5001 --debug  # This will auto-reload the server on code changes.
 =======
 
 # flask run --host=0.0.0.0 --port=5001 --debug  
 # # This will auto-reload the server on code changes.
->>>>>>> dedeepya
+>>>>>>> dedeep
 ###################################################    
